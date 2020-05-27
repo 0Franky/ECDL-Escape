86744889d7d1e6436eb2c30f6cb2a1e2a5963867
Tonywrall <ascardavilli@gmail.com>
2020-05-27 19:32:41 +0200

    Creato main menu
    Creata scena contenente i seguenti elementi:
    Main menu: Gioca Opzioni Livelli Esci;
    Menu opzioni: Volume e difficoltà (da impostare)
    Menu per selezione livello ( da impostare)

---
+++ ECDL-Escape Game/Assets/Scenes/MenuScript/ClickScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickScript : MonoBehaviour
+{
+    
+    public void CLickOnPlay()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ClickExit()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+   
+}
