ba9b792e3bb317057a4b4485d310720869796343
Tonywrall <ascardavilli@gmail.com>
2020-05-28 17:44:32 +0200

    Aggiunti script Livelli

---
+++ ECDL-Escape Game/Assets/Scenes/MenuScript/LevelNumberScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelNumberScript : MonoBehaviour
+{
+    private Text text;
+    private int sceneIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        sceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "Level" + sceneIndex;
+    }
+}
---
+++ ECDL-Escape Game/Assets/Scenes/PlayerScript/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float dirX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dirX = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(dirX * 5, 0);
+    }
+}
