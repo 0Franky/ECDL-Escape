8ca1f62e501c846d945bf49609f41b17bc19aaf8
Ca3rwin <ivan.valluzzi@gmail.com>
2020-05-29 02:17:28 +0200

    Update anim

--- ECDL-Escape Game/Assets/PlayerMovement.cs
+++ ECDL-Escape Game/Assets/PlayerMovement.cs
@@ -7,40 +7,40 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
     public Animator anmtr;
-
-    private int forceMov = 3;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int moveForce = 400;
+    public int upwardForce = 40;
 
     // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKey("d"))
-        {
+    void Update() {
+        if (Input.GetKey("d")) {
             //rb.transform.position = 
             rb.transform.localScale = new Vector3(1, 1, 1);
-            rb.AddForce(new Vector2(forceMov, 0));
+            rb.AddForce(new Vector2(moveForce * Time.deltaTime, 0));
             anmtr.SetInteger("Run", 1);
             Debug.Log("Down D");
         }
 
-        if (Input.GetKey("a"))
-        {
+        if (Input.GetKey("a")) {
             //rb.transform.position = 
             rb.transform.localScale = new Vector3(-1, 1, 1);
-            rb.AddForce(new Vector2(-forceMov, 0));
+            rb.AddForce(new Vector2(-moveForce * Time.deltaTime, 0));
             anmtr.SetInteger("Run", -1);
             Debug.Log("Down A");
         }
 
-        if (Input.GetKeyUp("d") || Input.GetKeyUp("a"))
-        {
-            anmtr.SetInteger("Run", 0);
-            Debug.Log("Up D");
+        if (Input.GetKeyDown("w")) {
+            rb.AddForce(new Vector2(0, upwardForce * Time.deltaTime), ForceMode2D.Impulse);
         }
 
+        //if (Input.GetKeyUp("d") || Input.GetKeyUp("a"))
+        //{
+        //    anmtr.SetInteger("Run", 0);
+        //    Debug.Log("Up D");
+        //}
+
+        if (rb.velocity.x <= 0.1 && rb.velocity.x >= -0.1) {
+            anmtr.SetInteger("Run", 0);
+            Debug.Log("Too little force");
+        }
     }
 }
