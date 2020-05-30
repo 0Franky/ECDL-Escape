0ceee4e6025a0a784d1b520e7dd1a6174b4783ce
Tonywrall <ascardavilli@gmail.com>
2020-05-30 17:50:25 +0200

    Completata la gestione audio per ogni livello

---
+++ ECDL-Escape Game/Assets/Scripts/MenuScript/VolumeScript.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeScript : MonoBehaviour
+{
+    private static readonly string FirstPlay = "FirstPlay";
+    private static readonly string BackgruondPref = "BackgruondPref";
+    private static readonly string SoundEffectPref = "SoundEffectPref";
+    private int firstPlayInt;
+    public Slider backgroundSlider, soundEffectsSlider;
+    private float backgroundFloat, soundEffectsFloat;
+    public AudioSource [] backgroundAudio;
+    public AudioSource[] soundEffectsAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        firstPlayInt = PlayerPrefs.GetInt(FirstPlay);
+
+        if(firstPlayInt == 0)
+        {
+            backgroundFloat = .125f;
+            soundEffectsFloat = .75f;
+            backgroundSlider.value = backgroundFloat;
+            soundEffectsSlider.value = soundEffectsFloat;
+            PlayerPrefs.SetFloat(BackgruondPref, backgroundFloat);
+            PlayerPrefs.SetFloat(SoundEffectPref, soundEffectsFloat);
+            PlayerPrefs.SetInt(FirstPlay, -1);
+        }
+        else
+        {
+            backgroundFloat = PlayerPrefs.GetFloat(BackgruondPref);
+            backgroundSlider.value = backgroundFloat;
+            soundEffectsFloat = PlayerPrefs.GetFloat(SoundEffectPref);
+            soundEffectsSlider.value = soundEffectsFloat;
+        }
+    }
+
+    public void SaveSoundSettings()
+    {
+        PlayerPrefs.SetFloat(BackgruondPref, backgroundSlider.value);
+        PlayerPrefs.SetFloat(SoundEffectPref, soundEffectsSlider.value);
+
+    }
+
+    private void OnApplicationFocus(bool focus)
+    {
+        if (!focus)
+        {
+            SaveSoundSettings();
+        }
+    }
+
+
+    public void UpdateSound()
+    {
+        for (int j = 0; j < backgroundAudio.Length; j++)
+        {
+            backgroundAudio[j].volume = backgroundSlider.value;
+        }
+
+        for(int i =0; i < soundEffectsAudio.Length; i++)
+        {
+            soundEffectsAudio[i].volume = soundEffectsSlider.value;
+        }
+
+    }
+}  
