dc4d815b9eaae8b16be46e3ee291898815c75d3c
Ca3rwin <ivan.valluzzi@gmail.com>
2020-06-01 05:28:50 +0200

    Impostata interazione primordiale con nemici

---
+++ ECDL-Escape Game/Assets/CombatManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatManager : MonoBehaviour {
+
+    public Rigidbody2D player;
+    public Rigidbody2D enemy;
+
+    // Update is called once per frame
+    void Update() {
+      
+    }
+}
---
+++ ECDL-Escape Game/Assets/CombatTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatTrigger : MonoBehaviour {
+
+    public PlayerMovement2 movement;
+    public Transform myPos;
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Player")) {
+            movement.inCombat = true;
+            movement.enemyPos = myPos;
+            Debug.Log("Sei entrato in combattimento");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision2) {
+        if (collision2.CompareTag("Player")) {
+            movement.inCombat = false;
+            Debug.Log("Sei uscito dal combattimento");
+        }
+    }
+}
