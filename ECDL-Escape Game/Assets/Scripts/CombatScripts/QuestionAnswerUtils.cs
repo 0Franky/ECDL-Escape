ec25437328b531b29f2e91e6806bf94963b27e3a
0Franky <frhaexin@gmail.com>
2020-06-03 03:23:12 +0200

    Update QuestionAnswerUtils.cs

--- ECDL-Escape Game/Assets/Scripts/CombatScripts/QuestionAnswerUtils.cs
+++ ECDL-Escape Game/Assets/Scripts/CombatScripts/QuestionAnswerUtils.cs
@@ -20,7 +20,7 @@ public class QuestionAnswerUtils : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start() {
-        setModule(1);
+        //setModule(1); /* PER TEST */
     }
 
     // Update is called once per frame
